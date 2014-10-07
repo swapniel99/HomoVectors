bb48a70dbad5bc3520211f6b9f151258cb86df72
Swapniel <swapniel99@gmail.com>
2014-10-08 03:13:10 +0530

    Converted 2d matrix to 1d array. Previously it was array of arrays.

--- Matrices.cpp
+++ Matrices.cpp
@@ -13,21 +13,24 @@ Matrix::Matrix(int n,int m)
 {
 	this->n=n;
 	this->m=m;
-	p = new int*[n];
-	for(int i=0;i<n;i++)
-	{
-		p[i]=new int[m];
-		for(int j=0;j<m;j++)
-			p[i][j]=0;
-	}
+	size=n*m;//
+//	p = new int*[n];
+	p = new int[size]();
+	
+//	for(int i=0;i<n;i++)
+//	{
+//		p[i]=new int[m];
+//		for(int j=0;j<m;j++)
+//			p[i][j]=0;
+//	}
 }
 
 Matrix::~Matrix()
 {
-	for(int i=0;i<n;i++)
-	{
-		delete [] p[i];
-	}
+//	for(int i=0;i<n;i++)
+//	{
+//		delete [] p[i];
+//	}
 	delete [] p;
 	printf("Destructor called.\n");
 }
@@ -36,22 +39,30 @@ Matrix::Matrix(const Matrix &m1)
 {
 	n=m1.n;
 	m=m1.m;
-	p = new int*[n];
-	for(int i=0;i<n;i++)
-	{
-		p[i]=new int[m];
-		for(int j=0;j<m;j++)
-			p[i][j]=m1.p[i][j];
-	}
+	size=m1.size;//
+//	p = new int*[n];
+	p = new int[size];
+	
+//	for(int i=0;i<n;i++)
+//	{
+//		p[i]=new int[m];
+//		for(int j=0;j<m;j++)
+//			p[i][j]=m1.p[i][j];
+//	}
+	for(int i=0;i<size;i++)
+		p[i]=m1.p[i];
+	
 	printf("Copy Constructor called.\n");
 }
 
 void Matrix::input()
 {
-	printf("Provide %d x %d matrix:\n",n,m);
-	for(int i=0;i<n;i++)
-		for(int j=0;j<m;j++)
-			scanf("%d",&p[i][j]);
+	printf("Provide a %d x %d matrix:\n",n,m);
+//	for(int i=0;i<n;i++)
+//		for(int j=0;j<m;j++)
+//			scanf("%d",&p[i][j]);
+	for(int i=0;i<size;i++)
+		scanf("%d",p+i);
 }
 
 void Matrix::print()
@@ -59,7 +70,8 @@ void Matrix::print()
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<m;j++)
-			printf("%d\t",p[i][j]);
+//			printf("%d\t",p[i][j]);
+			printf("%d\t",p[i*m+j]);
 		printf("\n");
 	}
 }
@@ -67,9 +79,11 @@ void Matrix::print()
 const Matrix Matrix::operator +(const Matrix &mat) const
 {
 	Matrix res(n,m);
-	for(int i=0;i<n;i++)
-		for(int j=0;j<m;j++)
-			res.p[i][j] = this->p[i][j]+mat.p[i][j];
+//	for(int i=0;i<n;i++)
+//		for(int j=0;j<m;j++)
+//			res.p[i][j] = this->p[i][j]+mat.p[i][j];
+	for(int i=0;i<size;i++)
+		res.p[i] = this->p[i]+mat.p[i];
 	return res;
 }
 
@@ -77,13 +91,21 @@ const Matrix Matrix::operator *(const Matrix &mat) const
 {
 	Matrix res(n,mat.m);
 	int sum;
+//	for(int j=0;j<n;j++)
+//		for(int k=0;k<mat.m;k++)
+//		{
+//			sum=0;
+//			for(int i=0;i<m;i++)
+//				sum+=p[j][i]*mat.p[i][k];
+//			res.p[j][k]=sum;
+//		}
 	for(int j=0;j<n;j++)
 		for(int k=0;k<mat.m;k++)
 		{
 			sum=0;
 			for(int i=0;i<m;i++)
-				sum+=p[j][i]*mat.p[i][k];
-			res.p[j][k]=sum;
+				sum+=p[j*m+i]*mat.p[i*mat.m+k];
+			res.p[j*mat.m+k]=sum;
 		}
 	return res;
 }
@@ -91,19 +113,24 @@ const Matrix Matrix::operator *(const Matrix &mat) const
 const Matrix Matrix::operator =(const Matrix &mat)
 {	
 	printf("Assignment called.\n");
-	for(int i=0;i<n;i++)
-		delete [] p[i];
+//	for(int i=0;i<n;i++)
+//		delete [] p[i];
 	delete [] p;
 	
 	n=mat.n;
 	m=mat.m;
-	p = new int*[n];
-	for(int i=0;i<n;i++)
-	{
-		p[i]=new int[m];
-		for(int j=0;j<m;j++)
-			p[i][j]=mat.p[i][j];
-	}
+	size=mat.size;//
+//	p = new int*[n];
+//	for(int i=0;i<n;i++)
+//	{
+//		p[i]=new int[m];
+//		for(int j=0;j<m;j++)
+//			p[i][j]=mat.p[i][j];
+//	}
+	p = new int[size];
+	for(int i=0;i<size;i++)
+		p[i]=mat.p[i];
+
 	return *this;
 }
 
@@ -112,7 +139,9 @@ const Matrix Matrix::operator ~() const
 	Matrix res(m,n);
 	for(int i=0;i<n;i++)
 		for(int j=0;j<m;j++)
-			res.p[j][i]=p[i][j];
+//			res.p[j][i]=p[i][j];
+			res.p[j*n+i]=p[i*m+j];
+	
 	return res;
 }
 
--- Matrix.h
+++ Matrix.h
@@ -13,10 +13,11 @@
 class Matrix
 {
 private:
-	int n,m;
-	int **p;
+	int n,m,size;
+//	int **p;
+	int *p;
 public:
-	Matrix() {n=m=0; p=NULL;}
+	Matrix() {n=m=size=0; p=NULL;}
 	Matrix(int n,int m);
 	Matrix(const Matrix &m1);
 	~Matrix();
@@ -26,7 +27,8 @@ public:
 	const Matrix operator *(const Matrix &m1) const;
 	const Matrix operator =(const Matrix &mat);
 	const Matrix operator ~() const;
-	const int operator ()(int i,int j) const {return p[i][j];}
+//	const int operator ()(int i,int j) const {return p[i][j];}
+	const int operator ()(int i,int j) const {return p[i*m+j];}
 };
 
 #endif /* MATRIX_H_ */
